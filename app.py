7cdac4fae8c4b911fb739aab6af271bfa5e1e99c
Chuppala Anusha <anusha.chuppala12@gmail>
2025-10-31 14:27:49 +0530

    added if-else conditions

--- app.py
+++ app.py
@@ -1,24 +1,62 @@
-print("hello world")
-#primitive
-a = 30
-print(type(a))
-b = "50"
-print(type(b))
-c = True
-print(type(c))
+# print("hello world")
+# #primitive
+# a = 30
+# print(type(a))
 
-#non-primitive
-d = 2+3j
-print(type(d))
-e = (23,18,43)
-print(type(e))
-f = [23,18,43]
-print(type(f))
+# b = "50"
+# print(type(b))
 
-j = {23,60,86}
-print(type(j))
-h = {id : '23'}
-print(type(h))
+# c = True
+# print(type(c))
+
+# #non-primitive
+# d = 2+3j
+# print(type(d))
+
+# e = (23,18,43)
+# print(type(e))
+
+# f = [23,18,43]
+# print(type(f))
+
+# j = {23,60,86}
+# print(type(j))
+
+# h = {id : '23'}
+# print(type(h))
+
+# l = [23,18,43] #tuples are not mutable
+# print(l[0])
+
+# f = [23,18,43]
+# f[2] = 34
+# print(f) 
+
+def app():
+    a = 20
+    b = 30
+    return print(a+b)
+app()
+
+
+age = 20
+age = int(input("Enter your age: "))
+if(age >= 18):
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
+
+temperature = 32
+temperature = int(input("Enter the temperature: "))
+if(temperature == 32):
+    print("room temperature")
+elif(temperature <= 32):
+    print("It's hot")
+else:
+    print("it's cold")
+
+
+
 
 
 
