97fe0c79b78723e42f36a3766bde4435936b1884
Chuppala Anusha <anusha.chuppala12@gmail>
2025-10-31 10:44:31 +0530

    added primitive an dnon-primitive data types

---
+++ app.py
@@ -0,0 +1,24 @@
+print("hello world")
+#primitive
+a = 30
+print(type(a))
+b = "50"
+print(type(b))
+c = True
+print(type(c))
+
+#non-primitive
+d = 2+3j
+print(type(d))
+e = (23,18,43)
+print(type(e))
+f = [23,18,43]
+print(type(f))
+
+j = {23,60,86}
+print(type(j))
+h = {id : '23'}
+print(type(h))
+
+
+
